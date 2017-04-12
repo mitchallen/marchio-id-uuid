0c16ac7be38bea3180a73bf54b5bcaa18d8c1ec7
Mitch Allen <mitch.c.allen@gmail.com>
2017-04-12 09:24:22 -0700

    init commit

---
+++ README.md
@@ -0,0 +1,61 @@
+marchio-id-uuid
+==
+PUT DESCRIPTION HERE
+--
+
+<p align="left">
+  <a href="https://travis-ci.org/mitchallen/marchio-id-uuid">
+    <img src="https://img.shields.io/travis/mitchallen/marchio-id-uuid.svg?style=flat-square" alt="Continuous Integration">
+  </a>
+  <a href="https://codecov.io/gh/mitchallen/marchio-id-uuid">
+    <img src="https://codecov.io/gh/mitchallen/marchio-id-uuid/branch/master/graph/badge.svg" alt="Coverage Status">
+  </a>
+  <a href="https://npmjs.org/package/marchio-id-uuid">
+    <img src="http://img.shields.io/npm/dt/marchio-id-uuid.svg?style=flat-square" alt="Downloads">
+  </a>
+  <a href="https://npmjs.org/package/marchio-id-uuid">
+    <img src="http://img.shields.io/npm/v/marchio-id-uuid.svg?style=flat-square" alt="Version">
+  </a>
+  <a href="https://npmjs.com/package/marchio-id-uuid">
+    <img src="https://img.shields.io/github/license/mitchallen/marchio-id-uuid.svg" alt="License"></a>
+  </a>
+</p>
+
+## Installation
+
+    $ npm init
+    $ npm install marchio-id-uuid --save
+  
+* * *
+
+## Usage
+
+## Testing
+
+To test, go to the root folder and type (sans __$__):
+
+    $ npm test
+   
+* * *
+ 
+## Repo(s)
+
+* [bitbucket.org/mitchallen/marchio-id-uuid.git](https://bitbucket.org/mitchallen/marchio-id-uuid.git)
+* [github.com/mitchallen/marchio-id-uuid.git](https://github.com/mitchallen/marchio-id-uuid.git)
+
+* * *
+
+## Contributing
+
+In lieu of a formal style guide, take care to maintain the existing coding style.
+Add unit tests for any new or changed functionality. Lint and test your code.
+
+* * *
+
+## Version History
+
+#### Version 0.1.0 
+
+* initial release
+
+* * *
---
+++ modules/index.js
@@ -0,0 +1,82 @@
+/**
+    Module: marchio-id-uuid
+    Author: Mitch Allen
+*/
+
+/*jshint node: true */
+/*jshint esversion: 6 */
+
+"use strict";
+
+/**
+ * Module
+ * @module marchio-id-uuid
+ */
+
+/**
+ * 
+ * Factory module
+ * @module marchio-id-uuid-factory
+ */
+
+ /** 
+ * Factory method 
+ * It takes one spec parameter that must be an object with named parameters
+ * @param {Object} spec Named parameters object
+ * @returns {Promise} that resolves to {module:marchio-id-uuid}
+ * @example <caption>Usage example</caption>
+    var factory = require("marchio-id-uuid");
+ 
+    factory.create({})
+    .then(function(obj) {
+        return obj.health();
+    })
+    .catch( function(err) { 
+        console.error(err); 
+    });
+ */
+module.exports.create = (spec) => {
+
+    return new Promise((resolve, reject) => {
+
+        spec = spec || {};
+
+        // reject("reason");
+
+        // private 
+        let _package = "marchio-id-uuid";
+
+        resolve({
+            // public
+            /** Returns the package name
+              * @function
+              * @instance
+              * @memberof module:marchio-id-uuid
+            */
+            package: () => _package,
+            /** Health check
+              * @function
+              * @instance
+              * @memberof module:marchio-id-uuid
+              * @example <caption>Usage Example</caption>
+                var factory = require("marchio-id-uuid");
+             
+                factory.create({})
+                .then(function(obj) {
+                    return obj.health();
+                })
+                .then(function(result) {
+                    console.log("HEALTH: ", result);
+                })
+                .catch( function(err) { 
+                    console.error(err); 
+                });
+            */
+            health: function() {
+                return new Promise((resolve,reject) => {
+                    resolve("OK");
+                });
+            }
+        });
+    });
+};
